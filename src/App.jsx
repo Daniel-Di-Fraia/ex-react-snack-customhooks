a6eab359f29a8e4f5b2f889bedf9974a65927062
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2026-01-13 13:56:33 +0100

    finalizzazione snack 3 (useCustomPointer() – Cambia il Cursore del Mouse)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 //importazione customhooks
 import useSwitch from './CustomHooks/useSwitch';
 import useDate from './CustomHooks/useDate';
+import useCustomPointer from './CustomHooks/useCustomPointer';
 
 function App() {
 
@@ -11,6 +12,9 @@ function App() {
 
   //data aggiornata ogni secondo
   const currentDate = useDate();
+
+  //puntatore personalizzato
+  const customPointer = useCustomPointer("🔥");
 
   return (
     <>
@@ -23,6 +27,11 @@ function App() {
         <h1>Data e ora attuali:</h1>
         <h2>{currentDate.data}</h2>
         <h2>{currentDate.ora}</h2>
+      </div>
+
+      <div>
+        <h1>Sposta il mouse per vedere il cursore personalizzato!</h1>
+        {customPointer}
       </div>
     </>
   )
---
+++ src/CustomHooks/useCustomPointer.jsx
@@ -0,0 +1,43 @@
+//import usestate e useeffect da react
+import { useState, useEffect } from "react";
+
+export default function useCustomPointer(obj) {
+
+    //variabile di stato per la posizione del mouse
+    const [mouse, setMouse] = useState({ x: 0, y: 0 });
+
+    //comp che si monta una sola volta, evento per aggiornare coordinate puntatore
+    useEffect(() => {
+
+        const handleMouseMove = (e) => {
+            setMouse({ x: e.clientX, y: e.clientY });
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+
+    }, []);
+
+    const CursoreVisuale = (
+        <div style={{
+            position: 'fixed',
+            top: mouse.y,
+            left: mouse.x,
+            pointerEvents: 'none',
+            transform: 'translate(-50%, -50%)',
+            fontSize: '50px',
+            zIndex: 999
+        }}>
+            {obj}
+        </div>
+    );
+
+
+    return CursoreVisuale;
+
+
+}
+
